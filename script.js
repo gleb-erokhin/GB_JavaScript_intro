e44982a18aa26d7bd66396934c95d0fe2d30b43c
gleb-erokhin <gleb.erokhin@gmail.com>
2024-03-05 20:56:25 +0300

    Задание 3

--- script.js
+++ script.js
@@ -61,16 +61,54 @@
 // 4. Создайте произвольный массив из 5 элементов, удалите из него два элемента. 
 // Проверьте, какое станет значение свойства length после этого.
 
-const arr =[];
+// const arr =[];
 
-for (let index = 0; index < 5; index++) {
-    arr.push(Math.floor(Math.random() * 100));
-}
-console.log(arr);
-console.log(arr.length);
-arr.pop();
-arr.pop();
-console.log(arr);
-console.log(arr.length);
+// for (let index = 0; index < 5; index++) {
+//     arr.push(Math.floor(Math.random() * 100));
+// }
+// console.log(arr);
+// console.log(arr.length);
+// arr.pop();
+// arr.pop();
+// console.log(arr);
+// console.log(arr.length);
 
 
+// 1. С помощью цикла for выведите в консоль числа от 11 до 33.
+
+// for (let index = 11; index < 33; index++) {
+//     console.log(index);
+// }
+
+// 2. С помощью цикла for выведите в консоль нечетные числа в промежутке от 1 
+// до 100 включительно.
+
+// for (let index = 1; index < 101; index++) {
+//     if (index % 2 !== 0) {
+//         console.log(index);
+//     } 
+// }
+
+// 2 вариант
+// for (let index = 1; index < 101; index += 2) {
+//     console.log(index);
+// }
+
+// 3. С помощью цикла for выведите в консоль числа от 100 до 0.
+
+// for (let index = 100; index > 0; index--) {
+//     console.log(index);
+// }
+
+// 4. Создать переменную с заданным числом. Умножайте число на 3 столько раз, 
+// пока результат умножения не станет больше 1000. Какое число получится? 
+// Посчитайте количество итераций, необходимых для этого.
+
+// const num = 105;
+// let count = 0;
+
+// for (let index = num; index <= 1000; index *= 3) {
+//     // console.log(index);
+//     count++;
+// }
+// console.log(count);
