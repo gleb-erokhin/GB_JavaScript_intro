eb70a843174369655ab2921cf094aac6d09cf784
gleb-erokhin <gleb.erokhin@gmail.com>
2024-03-07 20:45:13 +0300

    Задание 1

---
+++ style.js
@@ -0,0 +1,55 @@
+// Задание 1:
+ 
+// 1. Создайте объект с ключами от 1 до 7, в качестве значений содержащий имена 
+// дней недели. Выведите на экран “Вторник”.
+
+// const obj = {
+//     '1': 'Понедельник',
+//     '2': 'Вторник',
+//     '3': 'Среда',
+// };
+
+// console.log(obj[2]);
+
+// 2. Создайте объект user с ключами 'name', 'surname', 'age'. Выведите в консоль
+// фамилию, имя и возраст одной строкой.
+
+// const user = {
+//     name: "Павел", 
+//     surname: "Тарасов", 
+//     age: 33,
+// };
+
+// console.log(`${user.name} ${user.surname} ${user.age}`);
+
+// const user = {
+//     name: "Павел", 
+//     surname: "Тарасов", 
+//     age: 33,
+// };
+
+// console.log(user);
+
+// 3. Добавьте в объект user свойство отчество, которое пользователь должен 
+// ввести с клавиатуры.
+
+// const user = {
+//     name: "Павел", 
+//     secondName: prompt('enter sername: '),
+//     surname: "Тарасов", 
+//     age: 33,
+// };
+
+// console.log(`${user.name} ${user.secondName} ${user.surname} ${user.age}`);
+
+// 4. Удалите свойство surname.
+
+// const user = {
+//     name: "Павел", 
+//     secondName: prompt('enter sername: '),
+//     surname: "Тарасов", 
+//     age: 33,
+// };
+
+// delete user.surname;
+// console.log(`${user.name} ${user.secondName} ${user.surname} ${user.age}`);
