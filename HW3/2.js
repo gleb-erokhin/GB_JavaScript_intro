1d2d8552dc6e8aed21f741bbd50e6289ef6f082e
gleb-erokhin <gleb.erokhin@gmail.com>
2024-03-10 21:54:22 +0300

    корректировка задания 2 по замечаниям преподавателя.

--- HW3/2.js
+++ HW3/2.js
@@ -12,13 +12,13 @@
 const userNumber = prompt('Enter your salay GROS: ');
 
 // Делаем проверку на число, если все нормально, то вычисляем с помощью функции
-if (isNaN(userNumber)) {
+if (Number.isNaN(userNumber)) {
     console.log("Значение задано неверно");
 } else {
-    console.log("Размер заработной платы за вычетом налогов равен:", userRate(userNumber));
+    console.log(`Размер заработной платы за вычетом налогов равен: ${userRate(userNumber)}`);
 }
 
 // функция для вычисления процента
 function userRate(number) {
-    return number - (number * 0.13);
+    return Math.round(number - (number * 0.13));
 }
