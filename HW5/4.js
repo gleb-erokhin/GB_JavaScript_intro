4ac1bdf37ba1ff90083a8ae8294d644b2209b6fb
gleb-erokhin <gleb.erokhin@gmail.com>
2024-03-13 21:11:55 +0300

    задача 4 корректировака ответа

--- HW5/4.js
+++ HW5/4.js
@@ -31,8 +31,14 @@ const products = [
   },
 ];
 
+// обращение к элементам массива по ключам, для понимания работы кода
+console.log(products[1].id);
+console.log(products[1].price);
+console.log(products[1].photos);
+
 // выводим массив с ключами где есть фото
 const sortFilter = products.filter(function (item) {
+  // Смотрим есть ли photos - как ключ ,если да то нужны те, у которых длина больше 0)
   return "photos" in item && item.photos.length > 0;
 });
 
